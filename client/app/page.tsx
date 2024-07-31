1dfc20381d965801452cec954451159895882d2e
KarthikJR <karthikjrao69@gmail.com>
2024-07-31 13:42:19 +0530

    feat: Create landing page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,19 +1,32 @@
-import Image from "next/image";
+"use client";
 
-import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<ModeToggle />
-			<div className="flex flex-col items-center space-y-8">
-				<h1 className="text-4xl font-bold text-center">
-					Welcome to <span className="text-blue-500">clipNkeep</span>
-				</h1>
-				<p className="text-lg text-center">
-					A simple website to store your Links
-				</p>
-			</div>
+		<main className="flex min-h-screen flex-col items-center justify-center space-y-6">
+			<Image
+				src="/rotating_text.svg"
+				alt="Rotating image"
+				className="w-32 h-32"
+				width={1000}
+				height={1000}
+			/>
+			<h1 className="sm:text-7xl text-3xl text-dark dark:text-primary flex flex-col gap-2 font-extrabold text-center">
+				Organize Your Links <span>Effortlessly</span>
+			</h1>
+			<p className="text-lg font-light text-center">
+				Streamline Your Bookmarking Experience with Clip & Keep
+			</p>
+			<Button
+				onClick={() => (window.location.href = "/auth")}
+				className="rounded-full p-5 text-xs"
+				variant="default"
+			>
+				{" "}
+				Let&apos;s Gooo -{">"}
+			</Button>
 		</main>
 	);
 }
